fdde43fd223946c9410543296c71e6206b70a205
Diego <diego.p.campos@animaeducacao.com.br>
2023-07-17 19:00:15 -0300

    Criando Testes de Unidade

---
+++ api.Tests/Models/AlunoTestes.cs
@@ -0,0 +1,44 @@
+﻿using api.Models;
+using Xunit;
+
+namespace api.Tests.Models
+{
+    public class AlunoTest
+    {
+        [Fact(DisplayName = nameof(CreateAluno_ValidParameters_Success))]
+        public void CreateAluno_ValidParameters_Success()
+        {
+            // Arrange
+            var aluno = new Aluno
+            {
+                Id = 1,
+                Pessoa = new Pessoa(),
+                Matricula = "123456",
+                DataCadastro = new DateTime(2023, 7, 1)
+            };
+
+            // Act - No action required
+
+            // Assert
+            Assert.Equal(1, aluno.Id);
+            Assert.NotNull(aluno.Pessoa);
+            Assert.Equal("123456", aluno.Matricula);
+            Assert.Equal(new DateTime(2023, 7, 1), aluno.DataCadastro);
+        }
+
+        [Fact(DisplayName = nameof(CreateAluno_InvalidParameters_Failure))]
+        public void CreateAluno_InvalidParameters_Failure()
+        {
+            // Arrange
+            var aluno = new Aluno();
+
+            // Act - No action required
+
+            // Assert
+            Assert.Equal(0, aluno.Id);
+            Assert.Null(aluno.Pessoa);
+            Assert.Null(aluno.Matricula);
+            Assert.Equal(default(DateTime), aluno.DataCadastro);
+        }
+    }
+}

fd674370fe4bfb823734784f236f17de73720581
William Cleisson de Carvalho <williamcc89@gmail.com>
2023-07-17 11:33:00 -0300

    Ocultado senha do usuário no retorno da API.

--- api/Models/Usuario.cs
+++ api/Models/Usuario.cs
@@ -1,11 +1,17 @@
-﻿namespace api.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace api.Models
 {
     public class Usuario
     {
         public int Id { get; set; }
         public Pessoa Pessoa { get; set; }
         public string Login { get; set; }
+
+        [JsonIgnore]
         public string Senha { get; set; }
+
         public DateTime? DataUltimoAcesso { get; set; }
         public DateTime DataCadastro { get; set; }
     }

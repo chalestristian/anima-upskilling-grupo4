97971d2758ed91d2bbecaaa6f092f6dad6824657
MatheusChavesF <matheus.chaves@animaeducacao.com.br>
2023-07-24 11:17:04 -0300

    Correção Usuário

--- Webforms2/Models/Usuario.cs
+++ Webforms2/Models/Usuario.cs
@@ -8,6 +8,12 @@ namespace Webforms2.Models
     public class Usuario
     {
         public string Nome { get; set; }
-        public string Senha { get; set; }        
+        public string Senha { get; set; }
+
+        public Usuario(string nome, string senha)
+        {
+            Nome = nome;
+            Senha = senha;
+        }
     }
 }

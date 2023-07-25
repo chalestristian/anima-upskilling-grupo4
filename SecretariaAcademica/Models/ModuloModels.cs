8fd30a36b806903e8f31c98c5612f23b33d388be
MatheusChavesF <matheus.chaves@animaeducacao.com.br>
2023-07-25 13:52:02 -0300

    Secretaria-Academica

---
+++ SecretariaAcademica/Models/ModuloModels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Webforms2.Models
+{
+    public class ModuloModels
+    {
+        public int IdModulo { get; set; }
+        public CursoModels Curso { get; set; }
+        public string NomeModulo { get; set; }
+        public int CH { get; set; }
+    }
+}

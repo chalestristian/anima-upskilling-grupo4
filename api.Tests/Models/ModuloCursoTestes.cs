a618783af8ddcbac64f530673e30ad5f853c7853
William Cleisson de Carvalho <williamcc89@gmail.com>
2023-07-27 23:27:56 -0300

    Adicionado api.Tests

---
+++ api.Tests/Models/ModuloCursoTestes.cs
@@ -0,0 +1,41 @@
+﻿using api.Models;
+using Xunit;
+
+namespace api.Tests.Models
+{
+    public class ModuloCursoTestes
+    {
+        [Fact(DisplayName = nameof(CreateModuloCurso_ValidParameters_Success))]
+
+        public void CreateModuloCurso_ValidParameters_Success()
+        {
+            // Arrange
+            var moduloCurso = new ModuloCurso
+            {
+                Curso = new Curso(),
+                Nome = "Módulo 1",
+                CH = 40
+            };
+
+            // Act - No action required
+
+            // Assert
+            Assert.NotNull(moduloCurso.Curso);
+            Assert.Equal("Módulo 1", moduloCurso.Nome);
+            Assert.Equal(40, moduloCurso.CH);
+        }
+
+        [Fact(DisplayName = nameof(CreateModuloCurso_InvalidParameters_Failure))]
+        public void CreateModuloCurso_InvalidParameters_Failure()
+        {
+            // Arrange
+            var moduloCurso = new ModuloCurso();
+
+            // Act - No action required
+
+            // Assert
+            Assert.Null(moduloCurso.Curso);
+            Assert.Null(moduloCurso.Nome);
+        }
+    }
+}
